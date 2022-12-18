b7599aba9071f2d07728272c9fbf0085b10f6d35
Joao Paulo <jpsilva.math@icloud.com>
2022-12-18 14:25:01 -0400

    first commit
    Signed-off-by: Joao Paulo <jpsilva.math@icloud.com>

---
+++ segunda_tela.py
@@ -0,0 +1,94 @@
+# Atividade final de Alex'
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Atualizar(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(452, 551)
+        MainWindow.setStyleSheet("background-color: rgb(230, 150, 0);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(180, 240, 121, 51))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: rgb(65, 105, 255);\n"
+                                      "border-style: outset;\n"
+                                      "border-width: 2px;\n"
+                                      "border-radius: 15px;\n"
+                                      "border-color: black;")
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(110, 110, 271, 101))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        font.setKerning(False)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+                                    "border-radius: 15px;\n"
+                                    "border-style: outset;\n"
+                                    "border-width: 2px;")
+        self.lineEdit.setText("")
+        self.lineEdit.setFrame(False)
+        self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit.setObjectName("lineEdit")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(140, 250, 41, 31))
+        self.label_2.setStyleSheet("")
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("send.png"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(370, 0, 61, 51))
+        self.label_3.setStyleSheet("")
+        self.label_3.setText("")
+        self.label_3.setPixmap(QtGui.QPixmap("marcaifms.png"))
+        self.label_3.setScaledContents(True)
+        self.label_3.setWordWrap(True)
+        self.label_3.setObjectName("label_3")
+        self.label_2.raise_()
+        self.pushButton.raise_()
+        self.lineEdit.raise_()
+        self.label_3.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 452, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "ENVIAR"))
+        self.lineEdit.setToolTip(_translate(
+            "MainWindow", "<html><head/><body><p align=\"center\"><br/></p></body></html>"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "mensagem"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_Atualizar()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
